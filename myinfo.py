a974b079f03bc5e11ebe7aa37f12f03e6fca7859
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-01-18 12:56:08 -0500

    my information v1

---
+++ myinfo.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/Smith/AppData/Local/Programs/Python/Python312/myinfo.py ==
+ jalen Smith
+ 
+My address is Union New Jersey 07083
+ 
+9736092907
+>>> 
+=== RESTART: C:/Users/Smith/AppData/Local/Programs/Python/Python312/myinfo.py ==
+ jalen Smith
+ 
+My address is Union New Jersey 07083
+ 
+9736092907
