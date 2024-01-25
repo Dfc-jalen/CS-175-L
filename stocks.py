eb9fe134c9a6022be97d34efbd98f555cd6ff47e
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-01-25 13:00:11 -0500

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -9,14 +9,14 @@ sell = float(input('Enter sell price per share: '))
 print(' ')
 
 Amountpaid = (shares1*purchase)
-commision2 = (shares1*sell*com1)
-commision1 = shares1*purchase*com1
+commission2 = (shares1*sell*com1)
+commission1 = shares1*purchase*com1
 stock_sell = (shares1*sell)
 profit = stock_sell - Amountpaid - commision2 - commision1 
 
 print(f'Amount paid for the stock: ${Amountpaid:,.2f}')
-print(f'Commision paid on the purchase: ${commision1:,.2f}')
+print(f'Commission paid on the purchase: ${commission1:,.2f}')
 print(f'Amount the Stock sold for: ${stock_sell:,.2f}')
-print(f'Commision paid on the sale: ${commision2:,.2f}')
+print(f'Commission paid on the sale: ${commission2:,.2f}')
 print(f'Profit (or lose if negatve) : ${profit:,.2f}')
  
