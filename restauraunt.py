ca01fb0cec9f8f504b16b0d2863420aebedcde44
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-02-08 16:34:09 -0500

    This is the first version of the code

---
+++ restauraunt.py
@@ -0,0 +1,155 @@
+
+# print(Style.BRIGHT + Fore.RED + "CHEESY")
+
+
+print( '''                Here are the restaurant options:)''' + '\n'  + 
+'''
+1. Joe's gourmet burgers ---> Vegetarian: (No) Vegan: (No) Gluten-Free (No)
+
+2. Main street pizza company ---> Vegetarian: (Yes) Vegan: (No) Gluten-Free (Yes)
+
+3. Corner Cafe ---> Vegetarian: (Yes) Vegan: (Yes) Gluten-Free (Yes)
+
+4. Mama's Fine Italian ---> Vegetarian: (Yes) Vegan: (No) Gluten-Free (No)
+
+5. The Chef's Kitchen ---> Vegetarian: (Yes) Vegan: (Yes) Gluten-Free (Yes)''')
+
+print(' ')
+
+while True:
+  
+  vegetarian = input('              Is anyone in your party vegetarian?: ')
+  print(' ')
+  
+  vegan = input('                   Is anyone in your party vegan?: ')
+  print(' ')
+  
+  gluten_free = input('             Is anyone in your party gluten-free?: ')
+  print(' ')
+  
+  
+
+  if vegetarian.lower() == 'yes' and vegan.lower() == 'yes' and gluten_free.lower() == 'yes':
+    print(' ')
+    print(f'''              The restaurant options are: 
+    
+                                    Corner Cafe
+                                    
+                                       and 
+                                       
+                                  The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'yes' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
+    print(' ')
+    print(f'''               The restaurant options are:
+    
+                                      Corner Cafe
+                                     
+                                         and 
+                                         
+                                    The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'yes' and gluten_free.lower() == 'yes':
+    print(' ')
+    print(                f'''The restaurant options are: 
+    
+                                     Corner Cafe 
+                                     
+                                        and 
+                                        
+                                  The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
+    print(' ')
+    print(                f'''The restaurant options are: 
+    
+                                     Corner Cafe 
+                                     
+                                         and 
+                                         
+                                    The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'yes' and vegan.lower() == 'no' and gluten_free.lower() == 'yes':
+    print(' ')
+    print(              f'''The restaurant options are: 
+    
+                            Main Street Pizza Company 
+                                
+                                       and 
+                                       
+                                   Corner Cafe 
+                                   
+                                       and 
+                                       
+                                The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'no' and gluten_free.lower() == 'yes':
+    print(' ')
+    print(             f'''The restaurant options are: 
+    
+                            Main Street Pizza Company 
+                                
+                                      and 
+                                      
+                                  Corner Cafe 
+                                  
+                                      and 
+                                      
+                                The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'yes' and vegan.lower() == 'no' and gluten_free.lower() == 'no':
+    print(' ')
+    print(             f'''The restaurant options are: 
+    
+                               Mama's Fine Italian 
+                              
+                                     and 
+                                     
+                              Main Street Pizza Company 
+                              
+                                     and 
+                                     
+                                  Corner Cafe 
+                                
+                                     and 
+                                     
+                              The Chef's Kitchen
+    ''')
+    break
+
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'no' and gluten_free.lower() == 'no':
+    print(' ')
+    print(               f'''The restaurant options are: 
+    
+                                Joe's Gourmet Burgers 
+                                
+                                      and 
+                                      
+                                Mama's Fine Italian
+                                
+                                      and
+                                      
+                                Main Street Pizza Company
+                                
+                                      and 
+                                      
+                                   Corner Cafe
+       
+                                      and 
+                                      
+                                  Chef's Kitchen
+    ''')
+    break
+
