e7078882fad3b138f1aac0b37daa607bf20f339e
Dfc-jalen <146123090+Dfc-jalen@users.noreply.github.com>
2024-02-12 00:18:02 -0500

    Add files via upload

--- restauraunt.py
+++ restauraunt.py
@@ -1,13 +1,10 @@
-# CS 175 LAB
-# JALEN SMITH
-# PROFESSOR ECKERT 
-# Restaurant
+#Jalen Smith
+#11/30/2020
+#Mr. Eckert 
+#Computer Science
+#Cs 175L 
 
-
-# print(Style.BRIGHT + Fore.RED + "CHEESY")
-
-
-print( '''                Here are the restaurant options:)''' + '\n'  + 
+print( '''                Here are the restaurant options:)''' + '\n' +  
 '''
 1. Joe's gourmet burgers ---> Vegetarian: (No) Vegan: (No) Gluten-Free (No)
 
@@ -19,142 +16,205 @@ print( '''                Here are the restaurant options:)''' + '\n'  +
 
 5. The Chef's Kitchen ---> Vegetarian: (Yes) Vegan: (Yes) Gluten-Free (Yes)''')
 
-print(' ')
-
 while True:
-  
+  print(' ')
   vegetarian = input('              Is anyone in your party vegetarian?: ')
   print(' ')
-  
+  print( vegetarian)
+  print(' ')
+
   vegan = input('                   Is anyone in your party vegan?: ')
   print(' ')
-  
+  print( (vegan))
+  print(' ')
+
   gluten_free = input('             Is anyone in your party gluten-free?: ')
   print(' ')
-  
-  
+  print( (gluten_free))
+  print(' ')
+
 
   if vegetarian.lower() == 'yes' and vegan.lower() == 'yes' and gluten_free.lower() == 'yes':
-    print(' ')
-    print(f'''              The restaurant options are: 
     
-                                    Corner Cafe
-                                    
-                                       and 
-                                       
-                                  The Chef's Kitchen
-    ''')
-    break
-
-  elif vegetarian.lower() == 'yes' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
     print(' ')
-    print(f'''               The restaurant options are:
-    
+    print(f'''The restaurant options are: 
+  
                                       Corner Cafe
-                                     
+  
                                          and 
-                                         
+  
                                     The Chef's Kitchen
-    ''')
-    break
+      ''')
+    
+    
+    ask = (input('Do you want to see the restaurant options again?  yes or no): '))
+    
+    if ask == 'yes':
+      continue
+    if ask == 'no':
+      print('Alright have a good day!')
+      break
+  
+  elif vegetarian.lower() == 'yes' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
+      print(' ')
+      print( f'''The restaurant options are:
+  
+                                        Corner Cafe
+  
+                                           and 
+  
+                                      The Chef's Kitchen
+      ''')
+  ask1 = (input('Do you want to see the restaurant options again? (yes or no ): '))
 
+  if ask1 == 'yes':
+    continue
+  if ask1 == 'no':
+    print('Alright have a good day!')
+    break
+  
   elif vegetarian.lower() == 'no' and vegan.lower() == 'yes' and gluten_free.lower() == 'yes':
-    print(' ')
-    print(                f'''The restaurant options are: 
-    
-                                     Corner Cafe 
-                                     
-                                        and 
-                                        
-                                  The Chef's Kitchen
-    ''')
-    break
-
-  elif vegetarian.lower() == 'no' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
-    print(' ')
-    print(                f'''The restaurant options are: 
-    
-                                     Corner Cafe 
-                                     
-                                         and 
-                                         
+      print(' ')
+      print( f'''The restaurant options are: 
+  
+                                       Corner Cafe 
+  
+                                          and 
+  
                                     The Chef's Kitchen
-    ''')
-    break
+      ''')
+  ask1 = (input('Do you want to see the restaurant options again? (yes or no): '))
 
+    
+  if ask1 == 'yes':
+    continue
+  if ask1 == 'no':
+    print('Alright have a good day!')
+    break
+    
+      
+  
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'yes' and gluten_free.lower() == 'no':
+      print(' ')
+      print( f'''The restaurant options are: 
+  
+                                       Corner Cafe 
+  
+                                           and 
+  
+                                      The Chef's Kitchen
+      ''')
+  ask = (input('Do you want to see the restaurant options again? ( yes or no): '))
+  if ask == 'yes':
+    continue
+  if ask == 'no':
+    print('Alright have a good day!')
+    break
+      
+  
   elif vegetarian.lower() == 'yes' and vegan.lower() == 'no' and gluten_free.lower() == 'yes':
-    print(' ')
-    print(              f'''The restaurant options are: 
-    
-                            Main Street Pizza Company 
-                                
-                                       and 
-                                       
-                                   Corner Cafe 
-                                   
-                                       and 
-                                       
-                                The Chef's Kitchen
-    ''')
-    break
-
-  elif vegetarian.lower() == 'no' and vegan.lower() == 'no' and gluten_free.lower() == 'yes':
-    print(' ')
-    print(             f'''The restaurant options are: 
-    
-                            Main Street Pizza Company 
-                                
-                                      and 
-                                      
-                                  Corner Cafe 
-                                  
-                                      and 
-                                      
-                                The Chef's Kitchen
-    ''')
-    break
-
-  elif vegetarian.lower() == 'yes' and vegan.lower() == 'no' and gluten_free.lower() == 'no':
-    print(' ')
-    print(             f'''The restaurant options are: 
-    
-                               Mama's Fine Italian 
-                              
-                                     and 
-                                     
+      print(' ')
+      print( f'''The restaurant options are: 
+  
                               Main Street Pizza Company 
-                              
-                                     and 
-                                     
-                                  Corner Cafe 
-                                
-                                     and 
-                                     
-                              The Chef's Kitchen
-    ''')
+  
+                                         and 
+  
+                                     Corner Cafe 
+  
+                                         and 
+  
+                                  The Chef's Kitchen
+      ''')
+  ask = (input('Do you want to see the restaurant options again? yes or no): '))
+  if ask == 'yes':
+    continue
+  if ask == 'no':
+    print('Alright have a good day!')
     break
-
+      
+  
+  elif vegetarian.lower() == 'no' and vegan.lower() == 'no' and gluten_free.lower() == 'yes':
+      print(' ')
+      print( f'''The restaurant options are: 
+  
+                              Main Street Pizza Company 
+  
+                                        and 
+  
+                                    Corner Cafe 
+  
+                                        and 
+  
+                                  The Chef's Kitchen
+      ''')
+  ask = (input('Do you want to see the restaurant options again? yes or no: '))
+  if ask == 'yes':
+    continue
+  if ask == 'no':
+    print('Alright have a good day!')
+    break
+      
+  
+  elif vegetarian.lower() == 'yes' and vegan.lower() == 'no' and gluten_free.lower() == 'no':
+      print(' ')
+      print( f'''The restaurant options are: 
+  
+                                 Mama's Fine Italian 
+  
+                                       and 
+  
+                                Main Street Pizza Company 
+  
+                                       and 
+  
+                                    Corner Cafe 
+  
+                                       and 
+  
+                                The Chef's Kitchen
+      ''')
+  ask = (input('Do you want to see the restaurant options again? ( yes or no): '))
+  if ask == 'yes':
+    continue
+  if ask == 'no':
+    print('Alright have a good day!')
+    break
+      
+  
   elif vegetarian.lower() == 'no' and vegan.lower() == 'no' and gluten_free.lower() == 'no':
-    print(' ')
-    print(               f'''The restaurant options are: 
+      print(' ')
+      print( f'''The restaurant options are: 
+  
+                                  Joe's Gourmet Burgers 
+  
+                                        and 
+  
+                                  Mama's Fine Italian
+  
+                                        and
+  
+                                  Main Street Pizza Company
+  
+                                        and 
+  
+                                     Corner Cafe
+  
+                                        and 
+  
+                                    Chef's Kitchen
+      ''')
+  ask = (input('Do you want to see the restaurant options again? ( yes or no): '))
+  if ask == 'yes':
+    continue
     
-                                Joe's Gourmet Burgers 
-                                
-                                      and 
-                                      
-                                Mama's Fine Italian
-                                
-                                      and
-                                      
-                                Main Street Pizza Company
-                                
-                                      and 
-                                      
-                                   Corner Cafe
-       
-                                      and 
-                                      
-                                  Chef's Kitchen
-    ''')
+  if ask == 'no':
+    print('Alright have a good day!')
     break
-
+      
+  
+  
+  
+  
+  
+  
